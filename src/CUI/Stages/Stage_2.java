e4a3d3974f085f5fd319a3098e25194dd41da92d
Cody <cyvn.fantasy@gmail.com>
2019-03-26 15:40:05 +1300

    Minor changes to Stage_2's script

--- src/CUI/Stages/Stage_2.java
+++ src/CUI/Stages/Stage_2.java
@@ -6,190 +6,208 @@
 package CUI.Stages;
 
 import CUI.Entity_Package.Player;
+import CUI.GameOverScreen;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Class for the Stage 1 of the Game.
+ *
  * @author lyleb and khoap
  */
 public class Stage_2 extends Stage
 {
+    Player player;
     Random rand = new Random();
-    String userInput = "";
-    String guardMovement = "";
-    Scanner scan = new Scanner(System.in);
-    boolean isCompleted = true;
-    boolean isValid = false;
-    boolean isCorrect = false;
-    
-     /**
-      * Generates a number for movement creation
-      * 
-      * @return An int with values from 1 - 3
-      */
+    String userInput;
+    String guardMovement;
+    boolean isCompleted;
+    boolean isValid;
+    boolean isCorrect;
+
+    /**
+     * Generates a number for movement creation
+     *
+     * @return An integer with values from 1 - 3
+     */
     public int genNum()
     {
         int number = rand.nextInt(2) + 1;
-        
+
         return number;
     }
-    
-     /**
-      * Generates movements from random numbers
-      * 
-      * @return nothing.
-      */
+
+    /**
+     * Generates movements from random numbers
+     *
+     * @return nothing.
+     */
     private void genMove()
     {
         int x = genNum();
-        
-        switch(x){
-            case 1: 
-                System.out.println("The guard is moving away from you. You are free to move forward.");
+
+        switch (x)
+        {
+            case 1:
+                System.out.println("The guard in the middle is moving away from you, but there's still guards on the side.");
                 guardMovement = "S";
                 break;
             case 2:
-                System.out.println("The guard is coming from the right, avoid him!");
+                System.out.println("A guard is coming from the right, avoid him!");
                 guardMovement = "D";
                 break;
             case 3:
-                System.out.println("The guard coming from the left, avoid him!");
+                System.out.println("A guard iscoming from the left, avoid him!");
                 guardMovement = "A";
                 break;
             default:
                 guardMovement = "W";
         }
     }
-    
+
     /**
      * Checks to see if player has made the correct move.
-     * 
+     *
      * @return isCorrect boolean.
      */
     private boolean checkMove()
     {
-        this.isCorrect =false;
-        switch(guardMovement)
+        this.isCorrect = false;
+        switch (this.guardMovement)
         {
             case "S":
-                if(userInput.equalsIgnoreCase("W"))
+                if (this.userInput.equalsIgnoreCase("W"))
                 {
-                    isCorrect = true;
+                    this.isCorrect = true;
                     this.isCompleted = true;
-                    break;
                 }
                 else
                 {
                     System.out.println("");
-                    System.out.println("Game Over! You have been detected!");
-                    isCorrect = false;
+                    GameOverScreen.printGameOverScreen(this.player, "Game Over! You have been detected!");
+                    this.isCorrect = false;
                     this.isCompleted = false;
                 }
                 break;
             case "A":
-                if(userInput.equalsIgnoreCase("D"))
+                if (this.userInput.equalsIgnoreCase("D"))
                 {
-                    isCorrect = true;
+                    this.isCorrect = true;
                     this.isCompleted = true;
                 }
                 else
                 {
                     System.out.println("");
-                    System.out.println("Game Over! You have been detected!");
-                    isCorrect = false;
+                    GameOverScreen.printGameOverScreen(this.player, "Game Over! You have been detected!");
+                    this.isCorrect = false;
                     this.isCompleted = false;
                 }
                 break;
             case "D":
-                if(userInput.equalsIgnoreCase("A"))
+                if (userInput.equalsIgnoreCase("A"))
                 {
-                    isCorrect = true;
+                    this.isCorrect = true;
                     this.isCompleted = true;
                 }
                 else
                 {
                     System.out.println("");
-                    System.out.println("Game Over! You have been detected!");
-                    isCorrect = false;
+                    GameOverScreen.printGameOverScreen(this.player, "Game Over! You have been detected!");
+                    this.isCorrect = false;
                     this.isCompleted = false;
                 }
                 break;
             default:
-                isCorrect=false;
-                this.isCompleted=false;
+                this.isCorrect = false;
+                this.isCompleted = false;
                 break;
         }
         return isCorrect;
     }
-    
-     /**
+
+    /**
      * Create the content for stage 2.
      */
     @Override
     public void initiateStage(Player player)
     {
+        // Iniating the Stage's Default Attributes
+        Scanner scan = new Scanner(System.in);
+        this.userInput = "";
+        this.guardMovement = "";
+        this.isCompleted = true;
+        this.isValid = false;
+        this.isCorrect = false;
+        this.player = player;
         int successAttempt = 0;
-        try {
+        
+        // Start Stage
+        try
+        {
             System.out.println("You broke out of the cell! Now sneak past the guards and find your way out!");
             Thread.sleep(2000);
-            System.out.println();
             System.out.println("Watch the guards' movements and don't walk in the same direction they're heading!");
             Thread.sleep(2000);
             System.out.println("");
-            
-           
-            for (successAttempt = 0; successAttempt < 3; successAttempt ++)  //Generates 3 moves and check for input.
+            System.out.println("[MOVEMENT: A - MOVE LEFT | W - MOVE FORWARD | D - MOVE RIGHT]");
+
+            for (successAttempt = 0; successAttempt < 3; successAttempt++)  //Generates 3 moves and check for input.
             {
                 this.genMove();
-               
-                do {
-                    userInput = scan.nextLine();
-                    
-                    if(!"W".equalsIgnoreCase(userInput) && !"A".equalsIgnoreCase(userInput) && !"S".equalsIgnoreCase(userInput) && !"D".equalsIgnoreCase(userInput))
+                System.out.print("Action [A|W|D]: ");
+                do
+                {
+                    this.userInput = scan.nextLine();
+
+                    if (!"W".equalsIgnoreCase(this.userInput) && !"A".equalsIgnoreCase(this.userInput)
+                            && !"S".equalsIgnoreCase(this.userInput) && !"D".equalsIgnoreCase(this.userInput))
                     {
                         System.out.println("Invalid input! Try again!");
                     }
-                    
+
                     else
-                        isValid = true;
-                }while (!isValid);   //Validates user input
-               
-                if(checkMove())
                     {
-                        System.out.println("");
-                        System.out.println("You have avoided that guard. Keep moving!");
-                        System.out.println("");
-                        Thread.sleep(1000);
+                        this.isValid = true;
                     }
+                } while (!isValid);   //Validates user input
+
+                if (checkMove())
+                {
+                    System.out.println("");
+                    System.out.println("You have avoided that guard. Keep moving!");
+                    System.out.println("");
+                    Thread.sleep(1000);
+                }
                 else
+                {
                     successAttempt = successAttempt + 3;   //Adds 3 to successAttempt to force break the loop upon detection.
-                
+                }
             }
-            
-            if(this.isCompleted == true)    // checks for stage completion and iniate stage 3 if completed
+
+            if (this.isCompleted == true)    // checks for stage completion and iniate stage 3 if completed
             {
                 System.out.println("");
-                System.out.println("You have successfully avoided all three guards! Look for the exit now!");
-            
+                System.out.println("==============================================================================");
+                System.out.println("You have successfully avoided all three guards!");
+                
+                System.out.println("");
+                System.out.println("==============================================================================");
+
                 player.setCurrentStageLevel(new Stage_3());
                 player.getCurrentStage().initiateStage(player);
             }
-                
-        } 
-        catch (InterruptedException ex) 
+
+        }
+        catch (InterruptedException ex)
         {
             Thread.currentThread().interrupt(); // restore interrupted status
-        }    
+        }
     }
-   
-    
+}
 //    public static void main(String[] args)
 //    {
 //        Player test = new Player("Test");
 //        Stage stage2 = new Stage_2();
 //        stage2.initiateStage(test);
 //    }
-}
+
